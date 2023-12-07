28c491e1ef67f974c65f06dfcc79b2cc61ab009a
woody <woodysoil71@gmail.com>
2023-12-08 02:06:37 +0800

    add argon2idHasher class

---
+++ class/argon2idHasher/Argon2idHasher.h
@@ -0,0 +1,26 @@
+// Argon2idHasher.h
+
+#ifndef ARGON2ID_HASHER_H
+#define ARGON2ID_HASHER_H
+
+#include <string>
+#include <vector>
+
+class Argon2idHasher {
+public:
+    Argon2idHasher(uint32_t t_cost, uint32_t m_cost, uint32_t parallelism, const std::string& salt_hex, size_t hash_len);
+
+    std::string generateHash(const std::string& password);
+    bool verifyHash(const std::string& password, const std::string& hash);
+
+private:
+    uint32_t t_cost_;
+    uint32_t m_cost_;
+    uint32_t parallelism_;
+    std::vector<uint8_t> salt_;
+    size_t hash_len_;
+
+    std::vector<uint8_t> hexstr_to_bytes(const std::string& hexstr);
+};
+
+#endif // ARGON2ID_HASHER_H
