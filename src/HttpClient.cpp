0e71a74abf9f3eddcb17fd0e602cd814d7de4b77
woody <woodysoil71@gmail.com>
2024-04-05 12:59:20 +0800

    comment debug output

--- src/HttpClient.cpp
+++ src/HttpClient.cpp
@@ -14,21 +14,21 @@ HttpResponse HttpClient::HttpGet(const std::string &url, long timeout) {
         headers[header.first] = header.second;
     }
 
-    if (response.status_code == 0) {
-        std::cout << "HTTP GET Request to URL: " << url << std::endl;
-        std::cout << "Status Code: " << response.status_code << std::endl;
-        if (response.error) {
-            std::cout << "cpr::Error message: " << response.error.message
-                      << std::endl;
-        }
+    // if (response.status_code == 0) {
+    //     std::cout << "HTTP GET Request to URL: " << url << std::endl;
+    //     std::cout << "Status Code: " << response.status_code << std::endl;
+    //     if (response.error) {
+    //         std::cout << "cpr::Error message: " << response.error.message
+    //                   << std::endl;
+    //     }
 
-        std::cout << "Response Headers:" << std::endl;
-        for (const auto &header : headers) {
-            std::cout << header.first << ": " << header.second << std::endl;
-        }
+    //     std::cout << "Response Headers:" << std::endl;
+    //     for (const auto &header : headers) {
+    //         std::cout << header.first << ": " << header.second << std::endl;
+    //     }
 
-        std::cout << "Response Body Preview: " << response.text << std::endl;
-    }
+    //     std::cout << "Response Body Preview: " << response.text << std::endl;
+    // }
 
     return HttpResponse(response.status_code, response.text, headers);
 }
@@ -44,16 +44,16 @@ HttpResponse HttpClient::HttpPost(const std::string &url,
         headers[header.first] = header.second;
     }
 
-    std::cout << "HTTP POST Request to URL: " << url << std::endl;
-    std::cout << "Payload: " << payload.dump() << std::endl;
-    std::cout << "Status Code: " << response.status_code << std::endl;
+    // std::cout << "HTTP POST Request to URL: " << url << std::endl;
+    // std::cout << "Payload: " << payload.dump() << std::endl;
+    // std::cout << "Status Code: " << response.status_code << std::endl;
 
-    std::cout << "Response Headers:" << std::endl;
-    for (const auto &header : headers) {
-        std::cout << header.first << ": " << header.second << std::endl;
-    }
+    // std::cout << "Response Headers:" << std::endl;
+    // for (const auto &header : headers) {
+    //     std::cout << header.first << ": " << header.second << std::endl;
+    // }
 
-    std::cout << "Response Body Preview: " << response.text << std::endl;
+    // std::cout << "Response Body Preview: " << response.text << std::endl;
 
     return HttpResponse(response.status_code, response.text, headers);
 }
